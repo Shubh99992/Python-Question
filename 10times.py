e0f03c5aa8128ebae5a69cbf4a97f1d38a011c62
Shubham <105529358+Shubh99992@users.noreply.github.com>
2023-09-04 09:35:48 +0530

    Uploading stuffs to run.

---
+++ 10times.py
@@ -0,0 +1,37 @@
+#Print "Hello World with Python"
+i = 1
+while i<=10:
+    print ("Hello World with Python")
+    i=i+1
+
+#print counting frome 1 to 100
+n =100
+i=0
+while i<n:
+      i=i+1
+      print (i)
+
+#print odd no.and even no. between 1 to 100
+num1= int(input("Enter a Number: "))
+if num1 % 2:
+    print("odd")
+else:
+    print("even")
+
+
+#print table of 2
+num = 2
+i = 1 
+while i<=10:
+     print(num,"x",i,"=",num*i)
+     i=i+1
+
+#Print  multiplicaton table of a given no.
+num = int(input("Number: "))
+i = 1 
+while i<=10:
+     print(num,"x",i,"=",num*i)
+     i=i+1
+
+
+     
